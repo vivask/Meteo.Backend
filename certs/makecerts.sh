240f335cf7d6785d8acc285d5afe1df6c8047f68
Your Name <you@example.com>
2022-10-09 07:22:40 +0300

    initial commit

---
+++ certs/makecerts.sh
@@ -0,0 +1,1 @@
+/home/w/Projects/Meteo/Backend/certs/makecerts.sh
--- internal/entities/period.go
+++
@@ -1,8 +0,0 @@
-package entities
-
-import "time"
-
-type Period struct {
-	Begin time.Time `json:"begin"`
-	End   time.Time `json:"end"`
-}
